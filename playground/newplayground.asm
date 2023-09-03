a5e41489b74a953131d14399aa2bbcda0ac10315
Nha Hoang <hdnha11@gmail.com>
2023-09-03 10:52:42 +0700

    Add playground for quick experiments

---
+++ playground/newplayground.asm
@@ -0,0 +1,20 @@
+; Playground Program Template
+; New: make new
+; Build: make build
+; Run: make run
+
+section .data
+
+section .text
+  global _start
+
+_start:
+  nop
+; start playground
+;----------------------------------
+
+;----------------------------------
+; end playground
+  nop
+
+section .bss
